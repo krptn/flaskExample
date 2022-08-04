25b7ccd76a4543073cddcc255553c54d0a589c20
mbs9 <66876095+MBS9@users.noreply.github.com>
2022-08-04 15:17:50 +0200

    thisWorks

--- app.py
+++ app.py
@@ -1,43 +1,40 @@
 from flask import Flask, make_response, redirect, request, render_template, url_for
 from krypton.auth.users.userModel import standardUser
+from krypton.auth.users.bases import UserError
 from functools import wraps
 
 app = Flask(__name__)
 
 def login_required(func):
-    current_user:standardUser
     @wraps(func)
     def inner():
-        global current_user
         try:
             username = request.cookies.get('username')
             token = request.cookies.get('_kryptonAuthToken')
             current_user = standardUser(username)
             current_user.restoreSession(token)
-        except:
-            return render_template("index.html")
-        return func()
+        except UserError:
+            return make_response(redirect(url_for('index')))
+        return func(current_user)
     return inner
 
 def get_user(func):
-    current_user:standardUser
     @wraps(func)
     def inner():
-        global current_user
         try:
             username = request.cookies.get('username')
             token = request.cookies.get('_kryptonAuthToken')
             current_user = standardUser(username)
             current_user.restoreSession(token)
-            return func()
-        except:
-            return func()
+            return func(current_user)
+        except UserError:
+            return func(standardUser(None))
     return inner
 
 @app.route('/login')
 @get_user
-def login():
-    return render_template('login.html')
+def login(current_user):
+    return render_template('login.html', current_user = current_user)
 
 @app.route('/login', methods=['POST'])
 def login_post():
@@ -45,15 +42,15 @@ def login_post():
     password = request.form.get('password')
     user = standardUser(email)
     token = user.login(password)
-    resp = make_response(redirect(url_for('main.profile')))
+    resp = make_response(redirect(url_for('profile')))
     resp.set_cookie('username', email)
     resp.set_cookie('_kryptonAuthToken', token)
     return resp
 
 @app.route('/signup')
 @get_user
-def signup():
-    return render_template('signup.html')
+def signup(current_user):
+    return render_template('signup.html', current_user = current_user)
 
 @app.route('/signup', methods=['POST'])
 def signup_post():
@@ -63,23 +60,27 @@ def signup_post():
     user = standardUser(None)
     token = user.saveNewUser(email, password)
     user.setData("name", name)
-    resp = make_response(redirect(url_for('main.profile')))
+    resp = make_response(redirect(url_for('profile')))
     resp.set_cookie('username', email)
     resp.set_cookie('_kryptonAuthToken', token)
     return resp
 
 @app.route('/logout')
 @login_required
-def logout():
+def logout(current_user:standardUser):
     current_user.logout()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('index'))
+
+@app.route('/index')
+@get_user
+def index(current_user):
+    return render_template('index.html', current_user = current_user)
 
 @app.route('/')
-@get_user
-def index():
-    return render_template('index.html', current_user = current_user)
+def home():
+    return make_response(redirect(url_for('index')))
 
 @app.route('/profile')
 @login_required
-def profile():
-    return render_template('profile.html', name=current_user.userName)
+def profile(current_user):
+    return render_template('profile.html', name=current_user.userName, current_user = current_user)
